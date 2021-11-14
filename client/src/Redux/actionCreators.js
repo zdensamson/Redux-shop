0d818f9d8a645b7596db4d1fd2eefc0e52e317c1
Deku <zachdsamson@gmail.com>
2021-11-13 19:50:52 -0700

    wrapped app in redux provider

--- client/src/Redux/actionCreators.js
+++ client/src/Redux/actionCreators.js
@@ -1,60 +1,67 @@
 import { Actions } from "./constants";
 
-export const ActionCreators = {
+
     // cart actions
-    toggleCart = () => {
+export const toggleCart = () => {
         return {
             type: Actions.TOGGLE_CART
         };
-    },
-    addToCart = (product) => {
+    };
+
+export const addToCart = (product) => {
         return {
             type: Actions.ADD_TO_CART,
             payload: product
         };
-    },
-    addMultipleToCart = (products) => {
+    };
+
+export const addMultipleToCart = (products) => {
         return {
             type: Actions.ADD_MULTIPLE_TO_CART,
             payload: products
         };
-    },
-    removeFromCart = (_id) => {
+    };
+
+export const removeFromCart = (_id) => {
         return {
             type: Actions.REMOVE_FROM_CART,
             payload: _id
         };
-    },
-    updateCartQuantity = (_id, purchaseQty) => {
+    };
+
+export const updateCartQuantity = (_id, purchaseQty) => {
         return {
             type: Actions.updateCartQuantity,
             payload: {_id, purchaseQty}
         };
-    },
-    clearCart = () => {
+    }
+
+export const clearCart = () => {
         return {
             type: Actions.clearCart
         };
-    },
+    };
+
     // category action
-    updateCategories = (categories) => {
+export const updateCategories = (categories) => {
         return {
             type: Actions.UPDATE_CATEGORIES,
             payload: categories
         };
-    },
+    };
+
     // current category actions
-    updateCurrentCategory = (category) => {
+export const updateCurrentCategory = (category) => {
         return {
             type: Actions.UPDATE_CURRENT_CATEGORY,
             payload: category
         };
-    },
+    };
+
     // product action
-    updateProducts = (products) => {
+export const updateProducts = (products) => {
         return {
             type: Actions.UPDATE_PRODUCTS,
             payload: products
         }
-    }
-}
+    };
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -3,16 +3,14 @@ import { Link, useParams } from "react-router-dom";
 import { useQuery } from '@apollo/client';
 
 import Cart from "../components/Cart";
-import { useStoreContext } from "../utils/GlobalState";
-import {
-  REMOVE_FROM_CART,
-  UPDATE_CART_QUANTITY,
-  ADD_TO_CART,
-  UPDATE_PRODUCTS,
-} from "../utils/actions";
+
 import { QUERY_PRODUCTS } from "../utils/queries";
 import { idbPromise } from "../utils/helpers";
 import spinner from '../assets/spinner.gif'
+
+// redux dependencies:
+import { useSelector, useDispatch } from 'react-redux';
+import { addToCart, removeFromCart, updateCartQuantity, updateProducts} from '../Redux/actionCreators'
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
